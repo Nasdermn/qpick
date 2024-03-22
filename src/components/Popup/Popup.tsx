3bfc6c40dedac4a3209840160bd690b0e0b529a5
Nasdermn <thunderusnasdermn@yandex.ru>
2024-03-22 09:30:15 +0300

    qpick: v. 1.0.1 - feat: Закрытие попапа по нажатию клавиш

--- src/components/Popup/Popup.tsx
+++ src/components/Popup/Popup.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 import styles from './Popup.module.scss';
@@ -5,6 +6,23 @@ import { IPopupProps } from '../../interfaces';
 
 function Popup({ isOpen, onClose }: IPopupProps) {
   const { t } = useTranslation();
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === 'Escape' || event.key === 'Enter' || event.key === ' ') {
+        event.preventDefault();
+        onClose();
+      }
+    };
+
+    if (isOpen) {
+      document.addEventListener('keydown', handleKeyDown);
+    }
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [isOpen, onClose]);
 
   return (
     <div className={`${styles.popup} ${isOpen ? styles.popup_opened : ''}`}>
